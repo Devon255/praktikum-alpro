781b7db83cc13e5275a3b897c37b74f1d74b6082
Devon255 <devonnsp25@gmail.com>
2025-02-05 19:20:13 +0700

    keuntungan.py

--- keuntungan.py
+++ keuntungan.py
@@ -4,16 +4,14 @@ harga = 650000
 harga_naik1 = 685000
 harga_naik2 = 715000
 
-#pertama
-total_harga = harga * emas1 #25 x 650.000 (harga awal)
-harga_sekarang = harga_naik1 * emas1 #685.000 x 25
+total_harga = harga * emas1 
+harga_sekarang = harga_naik1 * emas1 
 
 keuntungan = harga_sekarang - total_harga 
 persentase = keuntungan/total_harga * 100
 
 
-#kedua
-harga_emas2 = emas2 * harga_naik1 #15 x 685.000 (beli lagi)
+harga_emas2 = emas2 * harga_naik1 
 total_harga_emas = total_harga + harga_emas2
 total_emas = emas1 + emas2
 harga_emas3 = total_emas * harga_naik2
