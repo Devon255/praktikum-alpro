18018d7049fde9b190739fe6dcfd60f34f64cd82
Devon255 <devonnsp25@gmail.com>
2025-04-24 13:33:12 +0700

    anagram.py

--- 08 Pengolahan String dan Regular Expression/anagram.py
+++ 08 Pengolahan String dan Regular Expression/anagram.py
@@ -4,11 +4,11 @@ def anagram(kata, perbandingan):
     
     if len(kata) != len(perbandingan):
         return False
-    
-    for huruf in kata:
-        if kata.count(huruf) != perbandingan.count(huruf):
-            return False
-    return True
+    else:
+        for huruf in kata:
+            if kata.count(huruf) != perbandingan.count(huruf):
+                return False
+        return True
 
 
 print(anagram("mata","atma"))
@@ -16,18 +16,3 @@ print(anagram("mata","maat"))
 print(anagram("mata","taam"))
 print(anagram("mata","tama"))
 print(anagram("mata","katma"))
-        
-
-# Misal: mata anagram dengan atma, maat, taam, tama
-
-
-
-
-    # kata = kata.lower()
-    # perbandingan = perbandingan.lower()
-    # # kata = kata.split()
-    
-    # if sorted(perbandingan) == sorted(kata):
-    #     return "anagram"
-    # else: 
-    #     return "bukan anagram"
